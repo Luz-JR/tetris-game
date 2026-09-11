ea9c475ac4df28a16cbceef7cbbd4a3e3f05846b
Luz-JR <luzjaramillorivero@blazermail.net>
2026-09-11 12:17:35 -0500

    h

--- Tetris game thing/Raylib-CPP-Starter-Template-for-VSCODE-V2-main/src/block.h
+++ Tetris game thing/Raylib-CPP-Starter-Template-for-VSCODE-V2-main/src/block.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <map>
 #include "position.h"
+#include "colors.h"
 
 class Block
 {
@@ -12,4 +13,5 @@ public:
 private:
   int cellSize;
   int rotationState;
+  std::vector<Color> colors;
 };

001c77986d1deaae75d4eea0f584ee54d343ee8c
Luz-JR <luzjaramillorivero@blazermail.net>
2026-09-12 22:58:17 -0500

    g

--- Tetris game thing/Raylib-CPP-Starter-Template-for-VSCODE-V2-main/src/main.cpp
+++ Tetris game thing/Raylib-CPP-Starter-Template-for-VSCODE-V2-main/src/main.cpp
@@ -1,6 +1,18 @@
 #include <raylib.h>
 #include "game.h"
 
+double lastUpdateTime = 0;
+
+bool EventTriggered(double interval)
+{
+    double currentTime = GetTime();
+    if(currentTime - lastUpdateTime >= interval)
+    {
+        lastUpdateTime = currentTime;
+        return true;
+    }
+    return false;
+}
 int main() 
 {
     Color darkBlue = {44, 44, 127, 255};
@@ -16,6 +28,12 @@ int main()
     while(WindowShouldClose()== false)
     {
         game.HandleInput();
+        if(EventTriggered(0.2))
+        {
+            game.MoveBlockDown();
+        }
+        
+        game.MoveBlockDown();
         BeginDrawing();
         ClearBackground(darkBlue);
         EndDrawing();
